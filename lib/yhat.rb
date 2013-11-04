87e5512da5e3524030db7c533030e260383996f5
Greg <greg@yhathq.com>
2013-11-04 11:35:08 -0500

    first commit

---
+++ lib/yhat.rb
@@ -0,0 +1,40 @@
+require 'rubygems'
+require 'json'
+
+
+class Yhat
+  # Class that can be used to access the Yhat API
+ 
+  def initialize(username, apikey, uri = 'api.yhathq.com')
+    @username = username
+    @apikey = apikey
+    @base_uri = uri
+
+    if @base_uri == 'api.yhathq.com'
+      @is_enterprise = false
+    else
+      @is_enterprise = true
+    end
+
+  end
+
+  # Make a prediction by calling Yhat via HTTP. You should pass both
+  # the name of the model you want to use to make a prediction as well
+  # as a JSON object (or a Hash) that contains the data requried to 
+  # make a prediction.
+  #
+  # @param modelname [String]
+  # @param data [Hash]
+  # @return [Hash]
+  def predict(modelname, data)
+    uri = URI.parse(@base_uri)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new("/models/" + modelname + "/")
+    request.add_field('Content-Type', 'application/json')
+    request.body = data
+    response = http.request(request)
+    data = response.body
+    JSON.parse(data)
+  end
+end
+
---
+++ yhat.gemspec
@@ -0,0 +1,17 @@
+Gem::Specification.new do |s|
+  s.name = %q{yhat}
+  s.version = "0.0.1"
+  s.date = %q{2013-11-04}
+  s.homepage = %q{http://github.com/yhat/yhat-ruby}
+  s.description = %q{A ruby wrapper for the Yhat API.}
+  s.authors = ["Greg Lamp"]
+  s.email = ["greg@yhathq.com"]
+  s.summary = %q{Call models from the Yhat API.}
+  s.files = [
+    "Gemfile",
+    "README.md",
+    "lib/yhat.rb"
+  ]
+  s.require_paths = ["lib"]
+  s.licenses = ["BSD"]
+end
